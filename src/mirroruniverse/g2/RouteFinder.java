db0ad8a399a00f1a38764e5121ea0f80eb8cfa60
cyan <cyan.dai@gmail.com>
2011-11-19 14:27:32 -0500

    Add Infrastructure

---
+++ src/mirroruniverse/g2/RouteFinder.java
@@ -0,0 +1,20 @@
+package mirroruniverse.g2;
+
+import mirroruniverse.sim.Player;
+
+public class RouteFinder implements Player {
+	Info info;
+	Explorer explorer;
+	RouteFinder routeFinder;
+	
+	
+	
+	@Override
+	public int lookAndMove(int[][] aintViewL, int[][] aintViewR) {
+		
+		return 0;
+	}
+	
+	
+
+}
