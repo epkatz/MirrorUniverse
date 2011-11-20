7e6563d0d5aca850cf3e11c9c6607cd86d9948b1
Eli Katz <friendlyneighborhoodkatz@gmail.com>
2011-11-20 14:55:17 -0500

    Resolved mrege problems

--- src/mirroruniverse/g2/WallFlower.java
+++ src/mirroruniverse/g2/WallFlower.java
@@ -7,18 +7,26 @@ public class WallFlower implements Player {
 	private Explorer explorer;
 	private Info info;
 	private RouteFinder routeFinder;
+	Map rightMap;
+	Map leftMap;
 	
 	public WallFlower() {
 		this.info = new Info();
 		this.explorer = new Explorer(this.info);
 		this.routeFinder = new RouteFinder(this.info);	
+		rightMap = new Map("Right");
+		leftMap = new Map("Left");
 	}
 
 	@Override
 	public int lookAndMove(int[][] aintViewL, int[][] aintViewR) {
-		// TODO Auto-generated method stub
+		leftMap.updateView(aintViewL);
+		rightMap.updateView(aintViewR);
+		return move();
+	}
+		
+	public int move() {
 		return 0;
 	}
-	
 	
 }
