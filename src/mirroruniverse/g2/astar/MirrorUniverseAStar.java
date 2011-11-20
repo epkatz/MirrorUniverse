36c720a63a05cb4e7cc6bb0b15f11b2d5c19fc15
cyan <cyan.dai@gmail.com>
2011-11-20 18:51:24 -0500

    merge A*

--- src/mirroruniverse/g2/astar/MirrorUniverseAStar.java
+++ src/mirroruniverse/g2/astar/MirrorUniverseAStar.java
@@ -1,33 +1,36 @@
 package mirroruniverse.g2.astar;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import mirroruniverse.g2.Map;
 import mirroruniverse.g2.Position;
+import mirroruniverse.sim.MUMap;
 
 public class MirrorUniverseAStar extends AStar<State> {
 	Map leftMap;
 	Map rightMap;
-	
+
 	public MirrorUniverseAStar(Map leftMap, Map rightMap) {
 		this.leftMap = leftMap;
 		this.rightMap = rightMap;
 	}
 
 	@Override
-	protected boolean isGoal(State node) {
+	public boolean isGoal(State node) {
 		// if we haven't found the exits yet
 		if (leftMap.exitPos == null || rightMap.exitPos == null)
 			return false;
-		
+
 		// now we know the position of the exits
 		Position posLeft = node.posLeft;
 		Position posRight = node.posRight;
 		Position exitLeft = leftMap.exitPos;
 		Position exitRight = rightMap.exitPos;
-		
+
 		// if they are the same, both left and right
-		if(posLeft.x == exitLeft.x && posLeft.y == exitLeft.y
+		if (posLeft.x == exitLeft.x && posLeft.y == exitLeft.y
 				&& posRight.x == exitRight.x && posRight.y == exitRight.y)
 			return true;
 		return false;
@@ -36,43 +39,52 @@ public class MirrorUniverseAStar extends AStar<State> {
 	@Override
 	protected Double g(State from, State to) {
 		// TODO Auto-generated method stub
-		
-		return 1.0;
+		return null;
 	}
 
 	@Override
 	protected Double h(State from, State to) {
 		// TODO Auto-generated method stub
-		double x1,x2,y1,y2,deltaX,deltaY,diagonal;
-		
-		
-		x1 = from.posLeft.x;
-		y1 = from.posLeft.y;
-		x2 = to.posLeft.x;
-		y2 = to.posLeft.y;
-		deltaX = Math.abs(x1 - x2);
-		deltaY = Math.abs(y1 - y2);
-		diagonal = Math.max(deltaX, deltaY);
-		//double orthogonal = Math.abs(deltaX - deltaY);
-		double distanceLeft = diagonal;//+ orthogonal;
-		
-		x1 = from.posRight.x;
-		y1 = from.posRight.y;
-		x2 = to.posRight.x;
-		y2 = to.posRight.y;
-		deltaX = Math.abs(x1 - x2);
-		deltaY = Math.abs(y1 - y2);
-		diagonal = Math.max(deltaX, deltaY);
-		//orthogonal = Math.abs(deltaX - deltaY);
-		double distanceRight = diagonal;//+ orthogonal;
-		
-		return Math.max(distanceLeft, distanceRight);
+		return null;
 	}
 
 	@Override
 	protected List<State> generateSuccessors(State node) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<State> successors = new LinkedList<State>();
 
+		Position posLeft = node.posLeft;
+		Position posRight = node.posRight;
+
+		for (int i = 0; i != MUMap.aintDToM.length; ++i) {
+			int[] aintMove = MUMap.aintDToM[i];
+			int deltaX = aintMove[0];
+			int deltaY = aintMove[1];
+
+			Position newPosLeft;
+			// if it's on the exit, don't move
+			if (leftMap.isExit(posLeft))
+				newPosLeft = new Position(posLeft.x, posLeft.y);
+			else {
+				newPosLeft = new Position(posLeft.x + deltaX, posLeft.y
+						+ deltaY);
+				if (!leftMap.isValid(newPosLeft)) {
+					newPosLeft.x -= deltaX;
+					newPosLeft.y -= deltaY;
+				}
+			}
+
+			Position newPosRight;
+			if (rightMap.isExit(posRight))
+				newPosRight = new Position(posLeft.x, posLeft.y);
+			else {
+				newPosRight = new Position(posRight.x + deltaX, posRight.y
+						+ deltaY);
+				if (!rightMap.isValid(newPosRight)) {
+					newPosRight.x -= deltaX;
+					newPosRight.y -= deltaY;
+				}
+			}
+		}
+		return successors;
+	}
 }
