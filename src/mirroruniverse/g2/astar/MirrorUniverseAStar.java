413681e5862c4e6985335a107836a4dd74958661
Your Name <thevalparekh@gmail.com>
2011-11-20 16:16:39 -0500

    G & H cal

--- src/mirroruniverse/g2/astar/MirrorUniverseAStar.java
+++ src/mirroruniverse/g2/astar/MirrorUniverseAStar.java
@@ -22,13 +22,37 @@ public class MirrorUniverseAStar extends AStar<State> {
 	@Override
 	protected Double g(State from, State to) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return 1.0;
 	}
 
 	@Override
 	protected Double h(State from, State to) {
 		// TODO Auto-generated method stub
-		return null;
+		double x1,x2,y1,y2,deltaX,deltaY,diagonal;
+		
+		
+		x1 = from.posLeft.x;
+		y1 = from.posLeft.y;
+		x2 = to.posLeft.x;
+		y2 = to.posLeft.y;
+		deltaX = Math.abs(x1 - x2);
+		deltaY = Math.abs(y1 - y2);
+		diagonal = Math.max(deltaX, deltaY);
+		//double orthogonal = Math.abs(deltaX - deltaY);
+		double distanceLeft = diagonal;//+ orthogonal;
+		
+		x1 = from.posRight.x;
+		y1 = from.posRight.y;
+		x2 = to.posRight.x;
+		y2 = to.posRight.y;
+		deltaX = Math.abs(x1 - x2);
+		deltaY = Math.abs(y1 - y2);
+		diagonal = Math.max(deltaX, deltaY);
+		//orthogonal = Math.abs(deltaX - deltaY);
+		double distanceRight = diagonal;//+ orthogonal;
+		
+		return Math.max(distanceLeft, distanceRight);
 	}
 
 	@Override
