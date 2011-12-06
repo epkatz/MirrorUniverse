ccc03ec1b02637d3557d627206087b752b100e4e
elikatz <friendlyneighborhoodkatz@gmail.com>
2011-12-05 20:10:12 -0500

    changed configs

--- src/mirroruniverse/g2/Config.java
+++ src/mirroruniverse/g2/Config.java
@@ -5,6 +5,6 @@ public class Config {
 	
 	public static int DEFAULT_QUEUE_SIZE = 1024;
 	
-	public static final int GUARANTEED_SIZE = 200;
+	public static final int GUARANTEED_SIZE = 250;
 	public static final int MAX_SIZE = GUARANTEED_SIZE * 2 + 1;
 }
